833f8fde7fc0f94f3f6aa0e45a783c76b3729bbd
barabas007 <shop.ru.shop@mail.ru>
2021-01-17 14:52:21 +0300

    adding a new field to the model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_16_154724) do
+ActiveRecord::Schema.define(version: 2021_01_17_112103) do
 
   create_table "courses", force: :cascade do |t|
     t.string "image"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_01_16_154724) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "public"
   end
 
 end
