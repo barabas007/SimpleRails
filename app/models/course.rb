c95ec239a82809665bb352d5f929885082ac1fce
barabas007 <shop.ru.shop@mail.ru>
2021-01-17 14:15:00 +0300

    check image fnd star

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,20 @@
 class Course < ApplicationRecord
+
+def star_number
+  self.star.blank? ? 1 : self.star
 end
+
+def cover
+  self.image.blank? ? "default.jpg" : self.image
+end
+end
+=begin 
+Или можно записать
+  if star.blank?
+    1
+  else
+    star
+  end
+=end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :courses
   get 'pages/about'
   get 'pages/location'
+  #get 'pages/home'
+  root 'pages#home'  #Альтернативный способ указать маршрут.
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
